a0ac281da19866763cf66812bc6550a7a0581361
Psousa92 <voltaire@live.com.pt>
2024-04-21 01:36:33 +0100

    UI update
    Application UI update

--- PRIMA/FormApplication.Designer.cs
+++ PRIMA/FormApplication.Designer.cs
@@ -28,19 +28,291 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            this.messageTBox = new MaterialSkin.Controls.MaterialTextBox();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.ChatListBox = new MaterialSkin.Controls.MaterialListBox();
+            this.materialButton2 = new MaterialSkin.Controls.MaterialButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toggleLightDarkModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chatRoomListView = new MaterialSkin.Controls.MaterialListView();
+            this.messageComboBox = new MaterialSkin.Controls.MaterialComboBox();
+            this.userLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.accountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usernameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.passwordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.emailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // materialButton1
+            // 
+            this.materialButton1.AutoSize = false;
+            this.materialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton1.Depth = 0;
+            this.materialButton1.HighEmphasis = true;
+            this.materialButton1.Icon = null;
+            this.materialButton1.Location = new System.Drawing.Point(721, 393);
+            this.materialButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton1.Name = "materialButton1";
+            this.materialButton1.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton1.Size = new System.Drawing.Size(72, 48);
+            this.materialButton1.TabIndex = 0;
+            this.materialButton1.Text = "Send";
+            this.materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton1.UseAccentColor = false;
+            this.materialButton1.UseVisualStyleBackColor = true;
+            // 
+            // messageTBox
+            // 
+            this.messageTBox.AnimateReadOnly = false;
+            this.messageTBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.messageTBox.Depth = 0;
+            this.messageTBox.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.messageTBox.LeadingIcon = null;
+            this.messageTBox.Location = new System.Drawing.Point(258, 393);
+            this.messageTBox.MaxLength = 50;
+            this.messageTBox.MouseState = MaterialSkin.MouseState.OUT;
+            this.messageTBox.Multiline = false;
+            this.messageTBox.Name = "messageTBox";
+            this.messageTBox.Size = new System.Drawing.Size(456, 50);
+            this.messageTBox.TabIndex = 1;
+            this.messageTBox.Text = "";
+            this.messageTBox.TrailingIcon = null;
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(242, 67);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(10, 374);
+            this.materialDivider1.TabIndex = 2;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // ChatListBox
+            // 
+            this.ChatListBox.BackColor = System.Drawing.Color.White;
+            this.ChatListBox.BorderColor = System.Drawing.Color.LightGray;
+            this.ChatListBox.Depth = 0;
+            this.ChatListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ChatListBox.Location = new System.Drawing.Point(259, 95);
+            this.ChatListBox.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ChatListBox.Name = "ChatListBox";
+            this.ChatListBox.SelectedIndex = -1;
+            this.ChatListBox.SelectedItem = null;
+            this.ChatListBox.Size = new System.Drawing.Size(534, 292);
+            this.ChatListBox.TabIndex = 3;
+            // 
+            // materialButton2
+            // 
+            this.materialButton2.AutoSize = false;
+            this.materialButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton2.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton2.Depth = 0;
+            this.materialButton2.HighEmphasis = true;
+            this.materialButton2.Icon = null;
+            this.materialButton2.Location = new System.Drawing.Point(7, 393);
+            this.materialButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton2.Name = "materialButton2";
+            this.materialButton2.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton2.Size = new System.Drawing.Size(228, 45);
+            this.materialButton2.TabIndex = 5;
+            this.materialButton2.Text = "Whisper";
+            this.materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton2.UseAccentColor = false;
+            this.materialButton2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.accountToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(3, 64);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(794, 28);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toggleLightDarkModeToolStripMenuItem,
+            this.logoutToolStripMenuItem,
+            this.disconnectToolStripMenuItem});
+            this.optionsToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // toggleLightDarkModeToolStripMenuItem
+            // 
+            this.toggleLightDarkModeToolStripMenuItem.Name = "toggleLightDarkModeToolStripMenuItem";
+            this.toggleLightDarkModeToolStripMenuItem.Size = new System.Drawing.Size(241, 24);
+            this.toggleLightDarkModeToolStripMenuItem.Text = "Toggle Light/Dark Mode";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(241, 24);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(241, 24);
+            this.disconnectToolStripMenuItem.Text = "Exit";
+            // 
+            // chatRoomListView
+            // 
+            this.chatRoomListView.AutoSizeTable = false;
+            this.chatRoomListView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.chatRoomListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.chatRoomListView.Depth = 0;
+            this.chatRoomListView.FullRowSelect = true;
+            this.chatRoomListView.HideSelection = false;
+            this.chatRoomListView.Location = new System.Drawing.Point(3, 155);
+            this.chatRoomListView.MinimumSize = new System.Drawing.Size(200, 100);
+            this.chatRoomListView.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.chatRoomListView.MouseState = MaterialSkin.MouseState.OUT;
+            this.chatRoomListView.Name = "chatRoomListView";
+            this.chatRoomListView.OwnerDraw = true;
+            this.chatRoomListView.Size = new System.Drawing.Size(232, 232);
+            this.chatRoomListView.TabIndex = 8;
+            this.chatRoomListView.UseCompatibleStateImageBehavior = false;
+            this.chatRoomListView.View = System.Windows.Forms.View.Details;
+            // 
+            // messageComboBox
+            // 
+            this.messageComboBox.AutoResize = false;
+            this.messageComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.messageComboBox.Depth = 0;
+            this.messageComboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.messageComboBox.DropDownHeight = 174;
+            this.messageComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.messageComboBox.DropDownWidth = 121;
+            this.messageComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.messageComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.messageComboBox.FormattingEnabled = true;
+            this.messageComboBox.IntegralHeight = false;
+            this.messageComboBox.ItemHeight = 43;
+            this.messageComboBox.Location = new System.Drawing.Point(7, 100);
+            this.messageComboBox.MaxDropDownItems = 4;
+            this.messageComboBox.MouseState = MaterialSkin.MouseState.OUT;
+            this.messageComboBox.Name = "messageComboBox";
+            this.messageComboBox.Size = new System.Drawing.Size(228, 49);
+            this.messageComboBox.StartIndex = 0;
+            this.messageComboBox.TabIndex = 4;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Depth = 0;
+            this.userLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.userLabel.Location = new System.Drawing.Point(258, 70);
+            this.userLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(36, 19);
+            this.userLabel.TabIndex = 9;
+            this.userLabel.Text = "User:";
+            // 
+            // accountToolStripMenuItem
+            // 
+            this.accountToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateInfoToolStripMenuItem});
+            this.accountToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accountToolStripMenuItem.Name = "accountToolStripMenuItem";
+            this.accountToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.accountToolStripMenuItem.Text = "Account";
+            // 
+            // updateInfoToolStripMenuItem
+            // 
+            this.updateInfoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usernameToolStripMenuItem,
+            this.nameToolStripMenuItem,
+            this.passwordToolStripMenuItem,
+            this.emailToolStripMenuItem});
+            this.updateInfoToolStripMenuItem.Name = "updateInfoToolStripMenuItem";
+            this.updateInfoToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.updateInfoToolStripMenuItem.Text = "Update Info";
+            // 
+            // usernameToolStripMenuItem
+            // 
+            this.usernameToolStripMenuItem.Name = "usernameToolStripMenuItem";
+            this.usernameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usernameToolStripMenuItem.Text = "Username";
+            // 
+            // nameToolStripMenuItem
+            // 
+            this.nameToolStripMenuItem.Name = "nameToolStripMenuItem";
+            this.nameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nameToolStripMenuItem.Text = "Name";
+            // 
+            // passwordToolStripMenuItem
+            // 
+            this.passwordToolStripMenuItem.Name = "passwordToolStripMenuItem";
+            this.passwordToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.passwordToolStripMenuItem.Text = "Password";
+            // 
+            // emailToolStripMenuItem
+            // 
+            this.emailToolStripMenuItem.Name = "emailToolStripMenuItem";
+            this.emailToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.emailToolStripMenuItem.Text = "Email";
             // 
             // FormApplication
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.chatRoomListView);
+            this.Controls.Add(this.materialButton2);
+            this.Controls.Add(this.messageComboBox);
+            this.Controls.Add(this.ChatListBox);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.messageTBox);
+            this.Controls.Add(this.materialButton1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormApplication";
             this.Text = "PRIMA";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialButton materialButton1;
+        private MaterialSkin.Controls.MaterialTextBox messageTBox;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private MaterialSkin.Controls.MaterialListBox ChatListBox;
+        private MaterialSkin.Controls.MaterialButton materialButton2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toggleLightDarkModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private MaterialSkin.Controls.MaterialListView chatRoomListView;
+        private MaterialSkin.Controls.MaterialComboBox messageComboBox;
+        private MaterialSkin.Controls.MaterialLabel userLabel;
+        private System.Windows.Forms.ToolStripMenuItem accountToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usernameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem passwordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem emailToolStripMenuItem;
     }
 }
