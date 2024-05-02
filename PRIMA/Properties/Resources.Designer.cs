cb87d92e2a6faac73bc2ede4da6da15dbfa7f6eb
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2024-05-02 20:25:51 +0100

    FolderUpdate

--- PRIMA/Properties/Resources.Designer.cs
+++ PRIMA/Properties/Resources.Designer.cs
@@ -63,36 +63,6 @@ namespace PRIMA.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap cog_35 {
-            get {
-                object obj = ResourceManager.GetObject("cog_35", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Hamburger_icon_svg {
-            get {
-                object obj = ResourceManager.GetObject("Hamburger_icon.svg", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap icons8_menu_50 {
-            get {
-                object obj = ResourceManager.GetObject("icons8-menu-50", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap icons8_settings_50 {
             get {
                 object obj = ResourceManager.GetObject("icons8-settings-50", resourceCulture);
